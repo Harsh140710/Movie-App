4986c04475688acd79f3b7ad6b7120ac0b25536f
Harsh140710 <sutharharsh108@gmail.com>
2025-10-28 11:20:35 +0530

    chore: update dependencies and add Tailwind CSS configuration
    - Added nativewind and tailwindcss to package.json.
    - Removed reset-project.js script as it is no longer needed.
    - Created tailwind.config.js for Tailwind CSS setup.
    - Updated tsconfig.json to include nativewind-env.d.ts.

---
+++ app/Onboarding.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const Onboaeding = () => {
+  return (
+    <View>
+      <Text>Onboarding</Text>
+    </View>
+  )
+}
+
+export default Onboaeding
+
+const styles = StyleSheet.create({})
